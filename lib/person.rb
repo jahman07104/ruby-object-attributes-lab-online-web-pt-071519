c7ffe0405f2b037d987fc503a28a9a316e77cea7
Patrick Harrison <jahman@bellatlantic.net>
2019-08-07 18:34:14 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,16 @@
+  def name
+    @name
+  end
+  def name=(dogs_name)
+    @name = dogs_name
+  end
+  def breed
+       @breed
+  end
+  def breed=(beagle)
+      @breed = beagle
+  end
+  
+end
+ name = Dog.new
+ name.name = "Fido" 
