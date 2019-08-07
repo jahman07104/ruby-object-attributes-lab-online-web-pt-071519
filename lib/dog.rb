ec86a24ed5680e0e0ae1480d0f5e07cdad508c74
Patrick Harrison <jahman@bellatlantic.net>
2019-08-07 18:26:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -6,12 +6,12 @@ class Dog
     @name = dogs_name
   end
   
- def name
-    @name
-  end
-  def name=(dogs_name)
-    @name = dogs_name
-  end
+# def name
+#     @name
+#   end
+#   def name=(dogs_name)
+#     @name = dogs_name
+#   end
   
 end
  name = Dog.new
